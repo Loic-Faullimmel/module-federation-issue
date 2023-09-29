167250b26f1c0942dff4a55cd076629ab3e189c4
Loïc FAULLIMMEL <loic.faullimmel@kuhn.com>
2023-09-29 10:06:40 +0200

    add children unexpected routes catcher

--- src/app/lazy-routing/lazy-routing-routing.module.ts
+++ src/app/lazy-routing/lazy-routing-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
         path: 'hey-patrick',
         component: HeyPatrickComponent
       },
+      {
+        path: '**',
+        redirectTo: '',
+      },
     ]
   },
   {
