5eb6a0a09043a454512e0e34458886b595fe7c9b
Loïc FAULLIMMEL <loic.faullimmel@kuhn.com>
2023-09-29 11:56:50 +0200

    (broken) packed AppComponent into web component manually
    Broken: the first page load don't route to empty path, only work when clicking on 'home' btn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmptyComponent } from './components/empty/empty.component';
 
 const routes: Routes = [
   {
     path: 'lazy-route',
     loadChildren: () => import('./lazy-routing/lazy-routing.module').then(m => m.LazyRoutingModule)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: EmptyComponent,
   },
   {
     path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { createCustomElement } from '@angular/elements';
+import { Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyComponent } from './components/empty/empty.component';
 import { LazyRoutingModule } from './lazy-routing/lazy-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    EmptyComponent,
   ],
   imports: [
     BrowserModule,
@@ -15,6 +19,15 @@ import { LazyRoutingModule } from './lazy-routing/lazy-routing.module';
     LazyRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private readonly injector: Injector, private readonly router: Router) {}
+
+  ngDoBootstrap() {
+    const ce = createCustomElement(AppComponent, {injector: this.injector});
+    customElements.define('mf-lazy-module-routing-element', ce);
+
+    // this.router.navigate(['/']);
+  }
+}
